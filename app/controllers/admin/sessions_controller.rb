aa06f3ef504e1089b60d247701463af73e27e9d9
EC2 Default User <ec2-user@ip-172-31-36-44.ec2.internal>
2022-03-30 14:38:53 +0000

    会員側ログインページ

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -24,15 +24,15 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  before_action :configure_permitted_parameters
+  # before_action :configure_permitted_parameters
 
-  def after_sign_in_path_for(resource)
-    about_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   about_path
+  # end
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up)
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up)
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_022002) do
+ActiveRecord::Schema.define(version: 2022_03_30_141148) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2022_03_26_022002) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_active"
+    t.boolean "is_active", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
