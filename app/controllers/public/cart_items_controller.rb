15518a0f86d5506f04b8cad7aab0ec9374eebed6
EC2 Default User <ec2-user@ip-172-31-36-44.ec2.internal>
2022-04-01 16:15:21 +0000

    カート追加作業中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,6 +7,17 @@ class Public::CartItemsController < ApplicationController
   
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+          redirect_to cart_items_path
+    else 
+          @cart_items = current_customer.cart_items.all
+          render "index"
+    end
   end
   
   def update
@@ -19,5 +30,11 @@ class Public::CartItemsController < ApplicationController
   
   def destroy_all
     
+  end
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount, :price)
   end
 end
