f1d50b573c579a5ae845fde81d40a8916a69bf4e
EC2 Default User <ec2-user@ip-172-31-36-44.ec2.internal>
2022-04-04 14:02:34 +0000

    おおよそ完了

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,8 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.all
+    @order_details = @order.order_details.all
+    @total = @order_details.inject(0){|sum, order_detail| sum + order_detail.subtotal}
+    @order.postage = 800
   end
 end
